4cede23ba14d52b495d95f775111a42a8845062f
YASAN <yasandev@gmail.com>
2022-02-13 21:19:38 +0330

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ def get_links(url):
     website_text = website.text
     soup = BeautifulSoup(website_text, "html.parser")
 
-    for link in soup.find_all('a'):
-        href = link.get('href')
-        links.append(href)
+    for link in soup.find_all('a',{"class":"footer__btn"}):
+        href = str(link.get('href'))
+        if ".apk" in href:
+            links.append(href)
 
     for link in links:
         print(link)
-
-    print(len(links))
 
 
 get_links(blue_bank_url)
